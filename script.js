4276c0a013b79466135f97c935cc62c06a86df9a
Daniel Czeschner <daniel.czeschner@web.de>
2022-02-20 10:38:43 +0100

    JS Selectors

---
+++ script.js
@@ -0,0 +1,13 @@
+const PLAYER1_CLASS = "x";
+const PLAYER2_CLASS = "0";
+
+const CELLS = document.querySelectorAll(".cell");
+const BOARD = document.getElementById("board");
+const SCREEN = document.getElementById("screen");
+const SCREEN_MSG = document.getElementById("screen-msg");
+const RESET = document.getElementById("reset");
+const RESTART = document.getElementById("restart");
+
+startGame();
+
+function startGame() {}
